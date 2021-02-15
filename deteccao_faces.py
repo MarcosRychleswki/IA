ef923ee45f8e5a0f8fe9823dbc29370d13fe547e
MarcosRychleswki <vinirychlewski@gmail.com>
2021-02-15 10:32:28 -0300

    iafaces

---
+++ deteccao_faces.py
@@ -0,0 +1,23 @@
+import cv2
+
+imagem = cv2.imread('pessoas.jpg')
+
+classificador = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+
+deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.09,
+                                           minNeighbors=5
+                                           )
+#detectMultiScale = faz a detecçao das faces
+#scaleFactor = tratar da escala(zoom)
+
+print(deteccoes)
+print(len(deteccoes))
+
+for (x, y, l, a) in deteccoes:
+    cv2.rectangle(imagem, (x, y), (x + l, y + a), (0,255,9), 2)
+
+cv2.imshow('Detector', imagem)
+cv2.waitKey(0)
+cv2.destroyWindow()
